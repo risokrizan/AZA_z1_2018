8171eb4b4e540596a60c306e8a6091b0265c6a9f
RisoKrizan <richard.krizan@outlook.com>
2018-04-11 09:46:25 +0200

    funguje maly test

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+#define INF 2147483647
 typedef struct QItem {
     int vrchol;
     struct QItem *dalsi;
@@ -16,12 +18,14 @@ typedef struct Queue {
 
 typedef struct Neighbours {
     int vrchol;
+    int parent;
     struct Neighbours *dalsi;
 } Susedia;
 
 int map[150][150];
 int n, m;
 int visited[22500];
+int parent[22500];
 int zac_x = -1, zac_y = -1, ciel_x = -1, ciel_y = -1;
 
 void vloz_queue(Q *q, int vrchol) {
@@ -65,13 +69,14 @@ void mozneIst(Susedia **prvy) {
 }
 
 
-void pridajSuseda(Susedia **prvy, int vrchol) {
+void pridajSuseda(Susedia **prvy, int vrchol, int parent) {
 
     Susedia *novy = (Susedia *) malloc(sizeof(Susedia));
 
     Susedia *akt = *prvy;
 
     novy->vrchol = vrchol;
+    novy->parent = parent;
     novy->dalsi = NULL;
 
     if (*prvy == NULL) {
@@ -82,6 +87,10 @@ void pridajSuseda(Susedia **prvy, int vrchol) {
         akt->dalsi = novy;
     }
 
+}
+
+int na_Druhu(int a) {
+    return a * a;
 }
 
 void priprav_mapu(int m, int n) {
@@ -103,17 +112,30 @@ void priprav_mapu(int m, int n) {
 
 void vypis_mapu(int m, int n) {
 
-    printf("\n");
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
+            //printf("%d",map[i][j]);
+
             if (map[i][j] == 0)
+                //x
                 printf("X");
             if (map[i][j] == 1)
+                //" "
                 printf(" ");
             if (map[i][j] == 2)
+                //"o"
                 printf("o");
+
         }
         printf("\n");
+    }
+}
+void vycisti_mapu(int m, int n) {
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            map[i][j] == -1;
+        }
     }
 }
 
@@ -121,7 +143,7 @@ Susedia *zisti_susedov(int aktVrchol, int predchvrchol) {
 
     Susedia *prvy = NULL;
 
-
+    printf("aktvrchol %d parent %d", aktVrchol, predchvrchol);
     int akt_y = aktVrchol / n;
     int akt_x = aktVrchol % n;
     int predch_y = predchvrchol / n;
@@ -131,45 +153,60 @@ Susedia *zisti_susedov(int aktVrchol, int predchvrchol) {
     int susedia_x = akt_x + posun_x;
     int susedia_y = akt_y + posun_y;
 
-    //rovnaka pozicia
-    if (map[susedia_y][susedia_x] == 1)
-        pridajSuseda(&prvy, susedia_y * n + susedia_x);
-    //vychod
-    if (susedia_x + 1 < n)
-        if (map[susedia_y][susedia_x + 1] == 1)
-            pridajSuseda(&prvy, (susedia_y) * n + (susedia_x + 1));
-    //severovychod
-    if (susedia_y - 1 > 0 && susedia_x + 1 < n)
-        if (map[susedia_y - 1][susedia_x + 1] == 1)
-            pridajSuseda(&prvy, (susedia_y - 1) * n + (susedia_x + 1));
-    //juhovychod
-    if (susedia_y + 1 < m && susedia_x + 1 < n)
-        if (map[susedia_y + 1][susedia_x + 1] == 1)
-            pridajSuseda(&prvy, (susedia_y + 1) * n + (susedia_x + 1));
-    //sever
-    if (susedia_y - 1 > 0)
-        if (map[susedia_y - 1][susedia_x] == 1)
-            pridajSuseda(&prvy, (susedia_y - 1) * n + susedia_x);
-    //severozpad
-    if (susedia_y - 1 > 0 && susedia_x - 1 > 0)
-        if (map[susedia_y - 1][susedia_x - 1] == 1)
-            pridajSuseda(&prvy, (susedia_y - 1) * n + (susedia_x - 1));
-    //zapad
-    if (susedia_x - 1 > 0)
-        if (map[susedia_y][susedia_x - 1] == 1)
-            pridajSuseda(&prvy, (susedia_y) * n + (susedia_x - 1));
-    //juhozapad
-    if (susedia_y + 1 < m && susedia_x - 1 > 0)
-        if (map[susedia_y + 1][susedia_x - 1] == 1)
-            pridajSuseda(&prvy, (susedia_y + 1) * n + (susedia_x - 1));
-    //juh
-    if (susedia_y + 1 < m)
-        if (map[susedia_y + 1][susedia_x] == 1)
-            pridajSuseda(&prvy, (susedia_y + 1) * n + (susedia_x));
 
+
+    //pridajSuseda(&prvy, akt_y * n + akt_x, predchvrchol);
+
+    //severozpad
+    if ((na_Druhu((susedia_y - 1) - akt_y) + na_Druhu((susedia_x - 1) - akt_x)) < 26)
+        if (susedia_y - 1 >= 0 && susedia_x - 1 >= 0)
+            if (map[susedia_y - 1][susedia_x - 1] == 1)
+                pridajSuseda(&prvy, (susedia_y - 1) * n + (susedia_x - 1), predchvrchol);
+    //sever
+    if ((na_Druhu((susedia_y - 1) - akt_y) + na_Druhu((susedia_x) - akt_x)) < 26)
+        if (susedia_y - 1 >= 0)
+            if (map[susedia_y - 1][susedia_x] == 1)
+                pridajSuseda(&prvy, (susedia_y - 1) * n + susedia_x, predchvrchol);
+    //severovychod
+    if ((na_Druhu((susedia_y - 1) - akt_y) + na_Druhu((susedia_x + 1) - akt_x)) < 26)
+        if (susedia_y - 1 >= 0 && susedia_x + 1 < n)
+            if (map[susedia_y - 1][susedia_x + 1] == 1)
+                pridajSuseda(&prvy, (susedia_y - 1) * n + (susedia_x + 1), predchvrchol);
+    //zapad
+    if ((na_Druhu((susedia_y) - akt_y) + na_Druhu((susedia_x - 1) - akt_x)) < 26)
+        if (susedia_x - 1 >= 0)
+            if (map[susedia_y][susedia_x - 1] == 1)
+                pridajSuseda(&prvy, (susedia_y) * n + (susedia_x - 1), predchvrchol);
+    //rovnaka pozicia
+    if ((na_Druhu((susedia_y) - akt_y) + na_Druhu((susedia_x) - akt_x)) < 26)
+        if (map[susedia_y][susedia_x] == 1) {
+            pridajSuseda(&prvy, susedia_y * n + susedia_x, predchvrchol);
+            //printf("Pridavam rovnaky bod: %d /n",susedia_y*n+susedia_x);
+        }
+    //vychod
+    if ((na_Druhu((susedia_y) - akt_y) + na_Druhu((susedia_x + 1) - akt_x)) < 26)
+        if (susedia_x + 1 < n)
+            if (map[susedia_y][susedia_x + 1] == 1)
+                pridajSuseda(&prvy, (susedia_y) * n + (susedia_x + 1), predchvrchol);
+    //juhozapad
+    if ((na_Druhu((susedia_y + 1) - akt_y) + na_Druhu((susedia_x - 1) - akt_x)) < 26)
+        if (susedia_y + 1 < m && susedia_x - 1 >= 0)
+            if (map[susedia_y + 1][susedia_x - 1] == 1)
+                pridajSuseda(&prvy, (susedia_y + 1) * n + (susedia_x - 1), predchvrchol);
+    //juh
+    if ((na_Druhu((susedia_y + 1) - akt_y) + na_Druhu((susedia_x) - akt_x)) < 26)
+        if (susedia_y + 1 < m)
+            if (map[susedia_y + 1][susedia_x] == 1)
+                pridajSuseda(&prvy, (susedia_y + 1) * n + (susedia_x), predchvrchol);
+    //juhovychod
+    if ((na_Druhu((susedia_y + 1) - akt_y) + na_Druhu((susedia_x + 1) - akt_x)) < 26)
+        if (susedia_y + 1 < m && susedia_x + 1 < n)
+            if (map[susedia_y + 1][susedia_x + 1] == 1)
+                pridajSuseda(&prvy, (susedia_y + 1) * n + (susedia_x + 1), predchvrchol);
 
     return prvy;
 }
+
 
 void bfs(int pocVrchol, Q *q) {
 
@@ -179,14 +216,22 @@ void bfs(int pocVrchol, Q *q) {
     visited[pocVrchol] = 1;
     vloz_queue(q, pocVrchol);
 
-    while (!q->posledny == NULL) {
-        predchVrchol = aktualnyVrchol;
-        aktualnyVrchol = vyber_queue(q);
-        printf("Visited %d\n", aktualnyVrchol);
-        map[aktualnyVrchol / n][aktualnyVrchol % n] = 2;
-        if (aktualnyVrchol == ciel_y * n + ciel_x)
-            break;
+    for (int i = 0; i < n * m; i++) {
+        parent[i] = -1;
+        visited[i]= 0;
+    }
+    parent[pocVrchol] = pocVrchol;
 
+    while (!q->posledny == NULL) {
+
+
+        aktualnyVrchol = vyber_queue(q);
+        predchVrchol = parent[aktualnyVrchol];
+        //printf("X%d Y%d\n", aktualnyVrchol % n, aktualnyVrchol / n);
+        //map[aktualnyVrchol / n][aktualnyVrchol % n] = 2;
+        if (aktualnyVrchol == ciel_y * n + ciel_x) {
+           break;
+        }
         //zisti susedov
         Susedia *temp = zisti_susedov(aktualnyVrchol, predchVrchol);
 
@@ -198,12 +243,22 @@ void bfs(int pocVrchol, Q *q) {
             if (visited[adjVertex] == 0) {
                 visited[adjVertex] = 1;
                 vloz_queue(q, adjVertex);
+                parent[adjVertex] = aktualnyVrchol;
             }
             temp = temp->dalsi;
         }
+
     }
 }
 
+void markni_cestu(int vrchol) {
+
+    map[vrchol / n][vrchol % n] = 2;
+    if (vrchol == NULL)
+        return;
+    markni_cestu(parent[vrchol]);
+
+}
 
 int main() {
 
@@ -221,9 +276,10 @@ int main() {
         q->prvy = NULL;
         q->posledny = NULL;
         bfs((zac_y * m) + zac_x, q);
-
+        markni_cestu(ciel_y * n + ciel_x);
         vypis_mapu(m, n);
-
+        vycisti_mapu(m,n);
+        printf("--------------------\n");
     }
     // sem napis svoje riesenie
 
